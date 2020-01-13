8a8186b8c0f8380dbdfcc460e10497e58a01b5f7
Junior <jorge.li2@hotmail.com>
2020-01-13 17:58:51 +0100

    update

--- event.py
+++ event.py
@@ -1,39 +1,11 @@
 import boto3
 import json
-"""logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-from urllib.parse import unquote_plus
 
-s3_client = boto3.client('s3')
-
-def upload(event, context):
-
-    logger.info('## ENVIRONMENT VARIABLES')
-    logger.info(os.environ)
-    logger.info('## EVENT')
-    logger.info(event)
-    
-    cloudsearch_client = boto3.client("cloudsearchdomain", endpoint_url="https://doc-webri-2dz3yckt2f5cjq7hcsbois6nw4.eu-west-1.cloudsearch.amazonaws.com")
-    
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = unquote_plus(record['s3']['object']['key'])
-        download_path = '/tmp/{}.json'.format(uuid.uuid4())
-        logger.info('## DOWNLOAD PATH')
-        logger.info(download_path)
-        s3_client.download_file(bucket, key, download_path)
-        f=open(download_path, "r")
-        if f.mode == 'r':
-           contents = f.read()
-           logger.info('## JSON CONTENT')
-           logger.info(contents)
-        f.close()
-        cloudsearch_client.upload_documents(documents=download_path, contentType='application/json')
-"""
 def test(event,context):
+    args= event['queryStringParameters']
+    prova=args.get('simple')
     body={
-        "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...",
-        #"input": event
+        "message": prova
     }
 
     response ={
@@ -46,7 +18,7 @@ def test(event,context):
 def search(event,context):
     client = boto3.client('cloudsearchdomain', endpoint_url='https://search-webri-2dz3yckt2f5cjq7hcsbois6nw4.eu-west-1.cloudsearch.amazonaws.com')
     args= event['queryStringParameters']
-    simple=args['simple']
+    simple=args.get('simple')
     if simple:
         response = client.search(
         query= simple,
@@ -55,6 +27,7 @@ def search(event,context):
         "statusCode":200,
         "body": json.dumps(response)}
         return ret
+    if
 
     ret={
         "statusCode":200,
