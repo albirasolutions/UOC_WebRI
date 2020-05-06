b6c2f08d5a46fed987f51025041fab525eac4995
Miquel Fonolleda <mfonolleda@albirasolutions.com>
2020-05-06 18:18:15 +0200

    Update bash deploy file

--- bin/cp-uoc-test.sh
+++ bin/cp-uoc-test.sh
@@ -3,14 +3,14 @@
 #
 # create tmp folder
 mkdir ../PLATACON_WEB_RI_TEST_TMP/
-cd ../PLATACON_WEB_RI_TEST/
 # copy content to tmp folder
-cp -r * ../PLATACON_WEB_RI_TEST_TMP/
-cd ../UOC_WebRI
-# copy new content to folder
-cp * -r ../PLATACON_WEB_RI_TEST/ 
 cd ../PLATACON_WEB_RI_TEST/
+cp -r * ../PLATACON_WEB_RI_TEST_TMP/
+# copy new content to folder
+cd ../UOC_WebRI
+cp * -r ../PLATACON_WEB_RI_TEST/ 
 # remove content folder
+cd ../PLATACON_WEB_RI_TEST/
 rm -r ./content
 rm -r ./static
 rm -r ./bin
@@ -18,8 +18,11 @@ rm -r ./bin
 cp -r ../PLATACON_WEB_RI_TEST_TMP/content/ .
 cp -r ../PLATACON_WEB_RI_TEST_TMP/static/ .
 cp ../PLATACON_WEB_RI_TEST_TMP/themes/uoc/static/admin/main.js ./themes/uoc/static/admin/main.js
+# copy content principal folder
+cd ../UOC_WebRI
+cp -r ./content/principal ../PLATACON_WEB_RI_TEST/content/principal
 # remove backup folder
 rm -r ../PLATACON_WEB_RI_TEST_TMP/
-git add -A 
-git commit -m "IECISA update on `date +'%Y-%m-%d %H:%M:%S'`";
-git push https://support%40albirasolutions.com:%3BO%3A56]%60%3E%2F06u63G@github.com/UOC/PLATACON_WEB_RI_TEST.git --all
+#git add -A 
+#git commit -m "IECISA update on `date +'%Y-%m-%d %H:%M:%S'`";
+#git push https://support%40albirasolutions.com:%3BO%3A56]%60%3E%2F06u63G@github.com/UOC/PLATACON_WEB_RI_TEST.git --all
