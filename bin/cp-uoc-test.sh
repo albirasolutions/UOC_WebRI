657393d6ec19bbaaec09d508e4bf90fc543e078c
Miquel Fonolleda <mfonolleda@albirasolutions.com>
2020-05-06 20:16:27 +0200

    Update bash file

--- bin/cp-uoc-test.sh
+++ bin/cp-uoc-test.sh
@@ -1,5 +1,7 @@
+##########################################################
 #
 #  CAREFUL!!!! This bash does not update the content/ and static/ folder, and the file ./themes/uoc/static/admin/main.js to prevent overwriting the plataconApiUrl variable
+#  Execute from ./UOC_WebRI path ==> ./bin/cp-uoc-test.sh
 #
 # create tmp folder
 mkdir ../PLATACON_WEB_RI_TEST_TMP/
@@ -20,9 +22,10 @@ cp -r ../PLATACON_WEB_RI_TEST_TMP/static/ .
 cp ../PLATACON_WEB_RI_TEST_TMP/themes/uoc/static/admin/main.js ./themes/uoc/static/admin/main.js
 # copy content principal folder
 cd ../UOC_WebRI
-cp -r ./content/principal ../PLATACON_WEB_RI_TEST/content/principal
+cp -r ./content/principal ../PLATACON_WEB_RI_TEST/content
 # remove backup folder
 rm -r ../PLATACON_WEB_RI_TEST_TMP/
-#git add -A 
-#git commit -m "IECISA update on `date +'%Y-%m-%d %H:%M:%S'`";
-#git push https://support%40albirasolutions.com:%3BO%3A56]%60%3E%2F06u63G@github.com/UOC/PLATACON_WEB_RI_TEST.git --all
+cd ../PLATACON_WEB_RI_TEST/
+git add -A 
+git commit -m "IECISA update on `date +'%Y-%m-%d %H:%M:%S'`";
+git push https://support%40albirasolutions.com:%3BO%3A56]%60%3E%2F06u63G@github.com/UOC/PLATACON_WEB_RI_TEST.git --all
