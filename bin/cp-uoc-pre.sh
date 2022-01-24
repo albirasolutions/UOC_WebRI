eb1892892db7384184d20564dd3ba6b4045ac514
Miquel Fonolleda <mfonolledara@uoc.edu>
2022-01-24 11:56:28 +0100

    Update deploy scripts

--- bin/cp-uoc-pre.sh
+++ bin/cp-uoc-pre.sh
@@ -6,30 +6,31 @@
 # create tmp folder
 mkdir ../PLATACON_WEB_RI_PRE_TMP/
 # copy content to tmp folder
-cd ../PLATACON_WEB_RI_PRE/
+cd ../Gitlab_UOC/PLATACON_WEB_RI_PRE/
 git pull
-cp -r * ../PLATACON_WEB_RI_PRE_TMP/
+cp -r * ../../PLATACON_WEB_RI_PRE_TMP/
 # copy new content to folder
-cd ../UOC_WebRI
-cp -r * ../PLATACON_WEB_RI_PRE/ 
+cd ../../UOC_WebRI
+cp -r * ../Gitlab_UOC/PLATACON_WEB_RI_PRE/ 
 # remove content folder
-cd ../PLATACON_WEB_RI_PRE/
+cd ../Gitlab_UOC/PLATACON_WEB_RI_PRE/
 rm -r ./content
 rm -r ./static
 rm -r ./bin
 # copy content folder from backup folder
-cp -r ../PLATACON_WEB_RI_PRE_TMP/content/ ./content
-cp -r ../PLATACON_WEB_RI_PRE_TMP/static/ ./static
-cp ../PLATACON_WEB_RI_PRE_TMP/themes/uoc/static/admin/main.js ./themes/uoc/static/admin/main.js
+cp -r ../../PLATACON_WEB_RI_PRE_TMP/content/ ./content
+cp -r ../../PLATACON_WEB_RI_PRE_TMP/static/ ./static
+cp ../../PLATACON_WEB_RI_PRE_TMP/themes/uoc/static/admin/main.js ./themes/uoc/static/admin/main.js
+cp ../../PLATACON_WEB_RI_PRE_TMP/themes/uoc/static/admin/config.yml ./themes/uoc/static/admin/config.yml
 # copy content principal and cercador folders
-cd ../UOC_WebRI
-cp -r ./content/principal ../PLATACON_WEB_RI_PRE/content
-cp -r ./content/cercador ../PLATACON_WEB_RI_PRE/content
-cp -r ./content/mapa-ambits-recerca ../PLATACON_WEB_RI_PRE/content
-cp -r ./content/difusions ../PLATACON_WEB_RI_PRE/content
+cd ../../UOC_WebRI
+cp -r ./content/principal ../Gitlab_UOC/PLATACON_WEB_RI_PRE/content
+cp -r ./content/cercador ../Gitlab_UOC/PLATACON_WEB_RI_PRE/content
+cp -r ./content/mapa-ambits-recerca ../Gitlab_UOC/PLATACON_WEB_RI_PRE/content
+cp -r ./content/difusions ../Gitlab_UOC/PLATACON_WEB_RI_PRE/content
 # remove backup folder
 rm -r ../PLATACON_WEB_RI_PRE_TMP/
-cd ../PLATACON_WEB_RI_PRE/
+cd ../Gitlab_UOC/PLATACON_WEB_RI_PRE/
 #git add -A 
 #git commit -m "INETUM update on `date +'%Y-%m-%d %H:%M:%S'`";
 #git push https://mfonolledara%40uoc.edu:vdk5fRxh3V8pyKq@github.com/UOC/PLATACON_WEB_RI_PRE.git --all
